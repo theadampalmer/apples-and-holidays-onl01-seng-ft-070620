ab11b0c38841f3ac090cf01fab52cc128ade8692
Adam Palmer <theadampalmer@gmail.com>
2020-07-15 20:40:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,10 +24,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
     holiday_hash[:winter][:new_years].push(supply)
 end
 
-
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
     holiday_hash[:spring][:memorial_day].push(supply)
 end
 
